2d97102c0fbadff783bd9696128aa7a2895aafc9
Lukee828 <lucius.bd@aol.com>
2025-10-09 17:41:59 +0200

    chore(ci): ping to satisfy required checks

--- src/datafeed/__init__.py
+++ src/datafeed/__init__.py
@@ -3,3 +3,5 @@ from .csv_source import CsvPriceSource
 
 __all__ = ["PriceBar", "PriceSource", "to_df", "CsvPriceSource", "YahooPriceSource"]
 from datafeed.yahoo_source import YahooPriceSource
+
+# ci-ping 2025-10-09T17:41:58
